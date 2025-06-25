419ecba38e550500a7483c8c7c9615899d0e1f12
RainLasea <1178391318@qq.com>
2025-06-25 08:58:16 +0800

    Create repositories

---
+++ src/main/java/com/abysslasea/anvilinnovate/Anvilinnovate.java
@@ -0,0 +1,12 @@
+package com.abysslasea.anvilinnovate;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Anvilinnovate.MODID)
+public class Anvilinnovate {
+    public static final String MODID = "anvilinnovate";
+
+    public Anvilinnovate() {
+
+    }
+}
